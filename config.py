1064a8dbb341f1aa3ed5d2c7998fe500dad8faa6
omkale221 <68939277+omkale221@users.noreply.github.com>
2020-08-31 13:59:45 +0530

    added param

--- config.py
+++ config.py
@@ -7,8 +7,18 @@ cuda_util = 98
 gpu_ram_util = 3256
 cpu_ram_util = 1258
 
-
 latency = 20
 batch_size = 1
+
 avg_fps = 32
 num_cams = 2
+
+dataset_model_version= "V_1.0"
+infer_framework= "pytorch"
+infer_lang= 'python'
+dataset_samples= 3434 #total test val. dataset images
+trained_model_version = "V_1.0"
+precision= 43
+recall= 44
+
+avg_accuracy= 80
