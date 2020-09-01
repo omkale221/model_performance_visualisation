ed1c6d8632ead501aa360f634da3f2840520567d
omkale221 <68939277+omkale221@users.noreply.github.com>
2020-09-01 13:15:42 +0530

    added more parameters

--- client.py
+++ client.py
@@ -3,6 +3,20 @@ import requests
 import json
 
 url = 'http://127.0.0.1:5000/'
-data = {'acc': config.accuracy, 'ram_util': config.cpu_ram_util, 'model_acc':config.model_acc, 'cpu_util':config.cpu_util, 'cuda_util':config.cuda_util}
+data = {'acc': config.accuracy, 'model_acc':config.model_acc, 'cpu_util':config.cpu_util, 'cuda_util':config.cuda_util,
+        'gpu_ram':config.gpu_ram_util , 'cpu_ram':config.cpu_ram_util, "latency":config.latency,
+         "batch_size":config.batch_size,
+         "avg_fps":config.avg_fps,
+         "num_cams":config.num_cams,
+         "dataset_model_version" : config.dataset_model_version,
+         "infer_framework" : config.infer_framework,
+        "infer_lang" : config.infer_lang,
+         "dataset_samples" :config.dataset_samples,
+         "trained_model_version" : config.trained_model_version,
+         "precision" : config.precision,
+         "recall" : config.recall,
+         "avg_accuracy": config.avg_accuracy
+
+        }
 js = json.dumps(data)
 x = requests.post(url, json = js)
